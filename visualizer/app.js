fd299a8aba6a54c561252a5ffcb5c0ece4b1b6e9
Paco <pakokrew@gmail.com>
2015-09-15 19:45:42 +0200

    Path vizu working

--- visualizer/app.js
+++ visualizer/app.js
@@ -4,30 +4,49 @@ app.controller("mainCtrl", function($scope, IAVizConnector) {
 
     IAVizConnector.getMap().then(function(data) {
         $scope.map = data.data;
+
+        IAVizConnector.getRoute().then(function(data) {
+            var path = data.data;
+            for(var i=0; i<path.length; i++) {
+                var cell = path[i];
+                $scope.map[cell.y][cell.x].isPath = true;
+            }
+        });
     });
 
+    var cellSize = 20;
+    var borderType = "2px solid black";
     $scope.getCellStyle = function(cell) {
         var style = {};
 
-        if(cell.occupantId)
-            style.backgroundColor = "yellow";
 
-        if(cell.isSheep)
-            style.backgroundColor = "red";
 
         if(cell.isPath)
             style.backgroundColor = "green";
 
+        if(cell.isSheep)
+            style.backgroundColor = "red";
+
+        if(cell.occupantId)
+            style.backgroundColor = "yellow";
+
+        style.left = cellSize * cell.x + 'px';
+        style.top = cellSize * cell.y + 'px';
+        style.width = cellSize + 'px';
+        style.height = cellSize + 'px';
+
+        if(cell.walls.top)
+            style.borderTop = borderType;
+        if(cell.walls.bottom)
+            style.borderBottom = borderType;
+        if(cell.walls.left)
+            style.borderLeft = borderType;
+        if(cell.walls.right)
+            style.borderRight = borderType;
+
         return style;
     };
 
-    IAVizConnector.getRoute().then(function(data) {
-        var path = data.data;
-        for(var i=0; i<path.length; i++) {
-            var cell = path[i];
-            $scope.map[cell.y][cell.x].isPath = true;
-        }
-    });
 });
 
 app.service("IAVizConnector", function($http) {
