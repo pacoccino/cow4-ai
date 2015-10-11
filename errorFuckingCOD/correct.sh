4e82f6fa4d4788e96d54540b9776de92da62e1f7
Paco <pakokrew@gmail.com>
2015-10-11 19:01:17 +0200

    Menage

--- errorFuckingCOD/correct.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cp easeljs-0.7.1.min.js ../node_modules/codeofwar/server/js/
-mkdir ../node_modules/codeofwar/server/img/
-cp ../node_modules/codeofwar/server/images/gameMap.png ../node_modules/codeofwar/server/img/
